bb3cf9fe656d01491e4611950e5f80c92efbb8e0
Ajil Abdalqader <ajilqado@gmail.com>
2020-06-03 00:27:28 +0200

    adding-summurayList

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,23 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
   return (
     <div className="card z-depth-0 project-summary">
-      <div className="card-content grey-text text-darken-3">
-        <span className="card-title ">Project title</span>
-        <p>Hello there</p>
-        <p className="grey-text">5 auguest, 1am</p>
-      </div>
-      <div className="card-content grey-text text-darken-3">
-        <span className="card-title ">Project title</span>
-        <p>Hello there</p>
-        <p className="grey-text">5 auguest, 1am</p>
-      </div>
-      <div className="card-content grey-text text-darken-3">
-        <span className="card-title ">Project title</span>
-        <p>Hello there</p>
-        <p className="grey-text">5 auguest, 1am</p>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title ">Project title</span>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit neque fugiat ullam consequuntur mollitia ipsam. Commodi mollitia velit tenetur amet unde rerum perferendis numquam deleniti adipisci, eligendi consectetur repudiandae natus.</p>
+        <p className="grey-text">3rd September, 2am</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary
