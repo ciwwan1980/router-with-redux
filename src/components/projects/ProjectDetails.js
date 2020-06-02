7f4a5752b4196fcafe9330f88d61c49ff167969c
Ajil Abdalqader <ajilqado@gmail.com>
2020-06-03 00:54:31 +0200

    adding-props.match.params-id-from-router

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 const ProjectDetails = (props) => {
  console.log(props, "props from router")
+   const id = props.match.params.id;
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project title -</span>
+          <span className="card-title">Project title -{id}</span>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et labore quaerat quibusdam vel saepe, ab voluptate accusantium culpa nemo fuga earum? Soluta amet nobis officia sed neque fuga aperiam quia?</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
