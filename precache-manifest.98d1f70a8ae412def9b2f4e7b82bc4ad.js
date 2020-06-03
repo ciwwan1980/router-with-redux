157e20e6befe74bae73ecf5f3cad2cccd6a7d363
Ajil Abdalqader <ajilqado@gmail.com>
2020-06-03 21:40:19 +0200

    Updates

---
+++ precache-manifest.98d1f70a8ae412def9b2f4e7b82bc4ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f255d9a7d8d1a44c2421dc1fb4b8686",
+    "url": "/simple-app/index.html"
+  },
+  {
+    "revision": "39577ec8a4dbbe965036",
+    "url": "/simple-app/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "1e12f209474141fb0093",
+    "url": "/simple-app/static/js/2.9ce2b224.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-app/static/js/2.9ce2b224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39577ec8a4dbbe965036",
+    "url": "/simple-app/static/js/main.702f6844.chunk.js"
+  },
+  {
+    "revision": "4cb4029c16e95a3b9cf7",
+    "url": "/simple-app/static/js/runtime-main.2546224d.js"
+  }
+]);
