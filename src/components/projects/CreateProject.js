d095532bb61b75a9a3746fbfcf14f6e1b33fe7fa
Ajil Abdalqader <ajilqado@gmail.com>
2020-06-07 22:57:44 +0200

    using-thunk

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,47 @@
+// import React, { Component } from 'react'
+
+// class CreateProject extends Component {
+//   state = {
+//     title: '',
+//     content: ''
+//   }
+//   handleChange = (e) => {
+//     this.setState({
+//       [e.target.id]: e.target.value
+//     })
+//   }
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log(this.state);
+//   }
+//   render() {
+//     return (
+//       <div className="container">
+//         <form className="white" onSubmit={this.handleSubmit}>
+//           <h5 className="grey-text text-darken-3">Create a New Project</h5>
+//           <div className="input-field">
+//             <input type="text" id='title' onChange={this.handleChange} />
+//             <label htmlFor="title">Project Title</label>
+//           </div>
+//           <div className="input-field">
+//             <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+//             <label htmlFor="content">Project Content</label>
+//           </div>
+//           <div className="input-field">
+//             <button className="btn pink lighten-1">Hello</button>
+//           </div>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
+
+// export default CreateProject
+
+
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions'
 
 class CreateProject extends Component {
   state = {
@@ -12,7 +55,8 @@ class CreateProject extends Component {
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    // console.log(this.state);
+    this.props.createProject(this.state);
   }
   render() {
     return (
@@ -28,7 +72,7 @@ class CreateProject extends Component {
             <label htmlFor="content">Project Content</label>
           </div>
           <div className="input-field">
-            <button className="btn pink lighten-1">Hello</button>
+            <button className="btn pink lighten-1">Create</button>
           </div>
         </form>
       </div>
@@ -36,4 +80,11 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject
+const mapDispatchToProps = dispatch => {
+  return {
+    createProject: (project) => dispatch(createProject(project))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject)
+
